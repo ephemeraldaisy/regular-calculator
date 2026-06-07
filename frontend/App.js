c6b8d26bb6ba16f1f78232c630a406a81084cb0e
ephemeraldaisy <rosequartz0251@gmail.com>
2026-06-07 16:56:54 +0900

    Change initial screen to CalculatorUI and update styles
    Replaced BudgetUI with CalculatorUI as the initial screen and updated styles.

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
-import BudgetUI from './components/BudgetUI';
+import CalculatorUI from './components/CalculatorUI'; // 가계부 대신 계산기가 첫 화면으로 오도록 교체
 import SecretUI from './components/SecretUI';
 
 export default function App() {
@@ -14,9 +14,10 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar barStyle={isVaultUnlocked ? "light-content" : "dark-content"} />
+      {/* 어두운 성인 사이트 톤 탈출을 위해 상단바를 화사하게 세팅 */}
+      <StatusBar barStyle="dark-content" />
       {!isVaultUnlocked ? (
-        <BudgetUI onUnlock={handleUnlock} />
+        <CalculatorUI onUnlock={handleUnlock} />
       ) : (
         <SecretUI token={wsToken} />
       )}
@@ -27,6 +28,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FAFAFA', // 산뜻한 미색 화이트로 변경
   },
 });
+
